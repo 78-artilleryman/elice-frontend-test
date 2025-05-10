f652baa541f60dbd8533c18b77cc937dedd3fecf
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-05-10 22:16:39 +0900

    feat: file 업로드 드래드앤드롭 방식 구현

--- src/components/FileUploader/FileUploader.tsx
+++ src/components/FileUploader/FileUploader.tsx
@@ -1,16 +1,19 @@
 import styles from "./FileUploader.module.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { BiImport, BiExport } from "react-icons/bi";
 import { FiFilePlus, FiFolderPlus } from "react-icons/fi";
 import { useFileSystem } from "../../contexts/FileSystemContext";
+
 export default function FileUploader() {
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
+  const [isDragging, setIsDragging] = useState(false);
   const { setRawFile } = useFileSystem();
 
-  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+  // 파일 등록
+  const handleFileSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
-    if (file) {
+    if (file && file.name.endsWith(".zip")) {
       setRawFile(file);
     }
   };
@@ -19,23 +22,48 @@ export default function FileUploader() {
     fileInputRef.current?.click();
   };
 
+  // 드래그 앤 드롭 관련 함수
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDragging(false);
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDragging(false);
+
+    const file = e.dataTransfer.files[0];
+    if (file && file.name.endsWith(".zip")) {
+      setRawFile(file);
+    }
+  };
+
   return (
-    <div className={styles.container}>
+    <div
+      className={`${styles.container} ${isDragging ? styles.dragging : ""}`}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+    >
       <input
         type="file"
         accept=".zip"
         ref={fileInputRef}
+        onChange={handleFileSelect}
         style={{ display: "none" }}
-        onChange={handleFileUpload}
       />
       <div className={styles.buttonContainer}>
-        <button className={styles.fileUploaderButton}>
+        <button className={styles.fileUploaderButton} onClick={openFileDialog}>
           <FiFilePlus />
         </button>
         <button className={styles.fileUploaderButton}>
           <FiFolderPlus />
         </button>
-        <button className={styles.fileUploaderButton} onClick={openFileDialog}>
+        <button className={styles.fileUploaderButton}>
           <BiImport />
         </button>
         <button
